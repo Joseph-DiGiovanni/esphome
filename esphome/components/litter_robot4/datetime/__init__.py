7d778d5b470a74e4edfde3bef30c08f8b825d73e
Joseph DiGiovanni <good.yard7176@jdigi.net>
2026-09-06 22:57:30 -0400

    Move sleep times to config category

--- esphome/components/litter_robot4/datetime/__init__.py
+++ esphome/components/litter_robot4/datetime/__init__.py
@@ -1,7 +1,12 @@
 import esphome.codegen as cg
 from esphome.components import datetime as datetime_
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, CONF_TYPE
+from esphome.const import (
+    CONF_ENTITY_CATEGORY,
+    CONF_ICON,
+    CONF_TYPE,
+    ENTITY_CATEGORY_CONFIG,
+)
 
 from .. import CONF_LITTER_ROBOT4_ID, LitterRobot4Component, litter_robot4_ns
 
@@ -35,6 +40,9 @@ _DAY_TYPES = {
 def _time_schema(*, icon=None):
     ext = {
         cv.GenerateID(CONF_LITTER_ROBOT4_ID): cv.use_id(LitterRobot4Component),
+        cv.Optional(
+            CONF_ENTITY_CATEGORY, default=ENTITY_CATEGORY_CONFIG
+        ): cv.entity_category,
     }
     if icon is not None:
         ext[cv.Optional(CONF_ICON, default=icon)] = cv.icon
